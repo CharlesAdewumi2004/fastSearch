bdccb5b9a588a8a502e14cdd1408c9a03fef28a5
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2025-12-29 20:07:51 +0000

    standard binary search for lower bound done

--- include/hybridSearch/details/lower-bound-impl.hpp
+++ include/hybridSearch/details/lower-bound-impl.hpp
@@ -6,6 +6,26 @@
 namespace hybrid_search::detail
 {
 
+    template <class Policy, class It, class V, class Comp>
+    It lower_bound_impl(Policy pol, It first, It last, const V &value, Comp comp)
+    {
+    }
+
+    template <class It, class V, class Comp>
+    inline It lower_bound_linear_scan(It first, typename std::iterator_traits<It>::difference_type count, const V &value, Comp comp)
+    {
+        while (count > 0)
+        {
+            if (!comp(*first, value))
+            {
+                break;
+            }
+            ++first;
+            --count;
+        }
+        return first;
+    }
+
     /* ============================================================
        RANDOM ACCESS ITERATORS
        ============================================================ */
@@ -29,7 +49,11 @@ namespace hybrid_search::detail
         }
     }
 
-    template <class RandomIt, class V, class Comp> 
+    /* ============================================================
+       STANDARD BINARY SEARCH
+       ============================================================ */
+
+    template <class RandomIt, class V, class Comp>
     inline RandomIt lower_bound_standard_binary_ra(RandomIt first, RandomIt last, const V &value, Comp comp)
     {
         using diff_t = typename std::iterator_traits<RandomIt>::difference_type;
@@ -39,6 +63,24 @@ namespace hybrid_search::detail
         {
             lower_bound_probe_ra(first, count, value, comp);
         }
+        return first;
+    }
+
+    /* ============================================================
+       HYBRID SEARCH
+       ============================================================ */
+
+    template <class RandomIt, class V, class Comp>
+    inline RandomIt lower_bound_hybrid_ra(size_t range, RandomIt first, RandomIt last, const V &value, Comp comp)
+    {
+        using diff_t = typename std::iterator_traits<RandomIt>::difference_type;
+
+        diff_t count = last - first;
+        while (count > range)
+        {
+            lower_bound_probe_ra(first, count, value, comp);
+        }
+        lower_bound_linear_scan(first, count, value, comp);
         return first;
     }
 
@@ -67,6 +109,10 @@ namespace hybrid_search::detail
         }
     }
 
+    /*  ============================================================
+        STANDARD BINARY SEARCH
+        ============================================================ */
+
     template <class ForwardIt, class V, class Comp>
     inline ForwardIt lower_bound_standard_binary_fw(ForwardIt first, ForwardIt last, const V &value, Comp comp)
     {
@@ -77,6 +123,24 @@ namespace hybrid_search::detail
         {
             lower_bound_probe_fw(first, count, value, comp);
         }
+        return first;
+    }
+
+    /* ============================================================
+       HYBRID SEARCH
+       ============================================================ */
+
+    template <class RandomIt, class V, class Comp>
+    inline RandomIt lower_bound_hybrid_fw(size_t range, RandomIt first, RandomIt last, const V &value, Comp comp)
+    {
+        using diff_t = typename std::iterator_traits<ForwardIt>::difference_type;
+
+        diff_t count = std::distance(first, last);
+        while (count > 0)
+        {
+            lower_bound_probe_fw(first, count, value, comp);
+        }
+        lower_bound_linear_scan(first, count, value, comp);
         return first;
     }
 
