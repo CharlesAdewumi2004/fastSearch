aa77f7b8b99e0c60cf21ca1e600ef1b1a1f9ee72
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2025-12-29 19:34:37 +0000

    standard binary search for lower bound done

--- include/hybridSearch/details/lower-bound-impl.hpp
+++ include/hybridSearch/details/lower-bound-impl.hpp
@@ -1,10 +1,83 @@
-#pragma once;
+#pragma once
 
-namespace hybridSearch::details
+#include <cstddef>
+#include <iterator>
+
+namespace hybrid_search::detail
 {
 
-    template <class Policy, class It, class V, class Comp>
-    It lower_bound_impl(Policy policy, It begin, It end, V &, Comp comp)
+    /* ============================================================
+       RANDOM ACCESS ITERATORS
+       ============================================================ */
+
+    template <class RandomIt, class V, class Comp>
+    inline void lower_bound_probe_ra(RandomIt &first, typename std::iterator_traits<RandomIt>::difference_type &count, const V &value, Comp comp)
     {
+        using diff_t = typename std::iterator_traits<RandomIt>::difference_type;
+
+        diff_t step = count / 2;
+        RandomIt mid = first + step;
+
+        if (comp(*mid, value))
+        {
+            first = mid + 1;
+            count -= step + 1;
+        }
+        else
+        {
+            count = step;
+        }
     }
+
+    template <class RandomIt, class V, class Comp> 
+    inline RandomIt lower_bound_standard_binary_ra(RandomIt first, RandomIt last, const V &value, Comp comp)
+    {
+        using diff_t = typename std::iterator_traits<RandomIt>::difference_type;
+
+        diff_t count = last - first;
+        while (count > 0)
+        {
+            lower_bound_probe_ra(first, count, value, comp);
+        }
+        return first;
+    }
+
+    /* ============================================================
+       FORWARD ITERATORS
+       ============================================================ */
+
+    template <class ForwardIt, class V, class Comp>
+    inline void lower_bound_probe_fw(ForwardIt &first, typename std::iterator_traits<ForwardIt>::difference_type &count, const V &value, Comp comp)
+    {
+        using diff_t = typename std::iterator_traits<ForwardIt>::difference_type;
+
+        diff_t step = count / 2;
+        ForwardIt mid = first;
+        std::advance(mid, step);
+
+        if (comp(*mid, value))
+        {
+            ++mid;
+            first = mid;
+            count -= step + 1;
+        }
+        else
+        {
+            count = step;
+        }
+    }
+
+    template <class ForwardIt, class V, class Comp>
+    inline ForwardIt lower_bound_standard_binary_fw(ForwardIt first, ForwardIt last, const V &value, Comp comp)
+    {
+        using diff_t = typename std::iterator_traits<ForwardIt>::difference_type;
+
+        diff_t count = std::distance(first, last);
+        while (count > 0)
+        {
+            lower_bound_probe_fw(first, count, value, comp);
+        }
+        return first;
+    }
+
 }
